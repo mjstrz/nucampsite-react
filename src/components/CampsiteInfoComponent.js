0f1f1539c13f2ee5ebf5dd0bb9540c679caebea6
mjstrz <strzelecki.monica7@gmail.com>
2022-01-06 23:11:12 -0500

    Fetch Post Comment - Week 5 Redux - 1.6.2022 23:11

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,7 +22,7 @@ function RenderCampsite({campsite}) {
         );
     }
 
-function RenderComments({comments, addComment, campsiteId}) {  
+function RenderComments({comments, postComment, campsiteId}) {  
  
          if (comments) {
              return(
@@ -34,7 +34,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                          <br/><br/>
                          </div>
                      </div>)}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
                  </div>
              )
              
@@ -82,7 +82,7 @@ function CampsiteInfo(props) {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                             comments={props.comments} 
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             campsiteId={props.campsite.id}
                         />
                     </div>
@@ -114,7 +114,7 @@ class CommentForm extends Component {
     handleSubmit(values) {
         this.toggleModal();
         //Week 5 Redux ex. 3
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is ' + JSON.stringify(values));
     }
